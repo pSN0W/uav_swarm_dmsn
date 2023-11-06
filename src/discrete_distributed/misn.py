b2276d864934a9bcf51be4a67094e403de7b9d66
psn0w <08yaksh08@gmail.com>
2023-11-07 04:54:00 +0530

    node classification algorithm

--- src/discrete_distributed/misn.py
+++ src/discrete_distributed/misn.py
@@ -41,6 +41,43 @@ class MISN:
                 points.append(point)
 
         return [SinkNode(point[0], point[1]) for point in points]
+    
+    def sink_node_classification(self, r: float) -> Tuple[List[Tuple[SinkNode,Set[SinkNode]]], List[Tuple[SinkNode,Set[SinkNode]]]]:
+        """classifies non overlapping sinknode in high and low density regions
+
+        Args:
+            r (float): The projection of drone on grpund
+
+        Returns:
+            Tuple[List[Tuple[SinkNode,Set[SinkNode]]], List[Tuple[SinkNode,Set[SinkNode]]]: Set of High and Low sink nodes. In the list you keep the representative sink node and set of all points in its neighborhood
+        """
+        sink_node_neighborhood: List[Tuple[SinkNode,Set[SinkNode]]] = []
+        
+        # Add the sink nodes below distance r in same neighborhood
+        for i,current_sink_node in enumerate(self.sink_nodes):
+            current_sink_node_nbrhood = set([current_sink_node])
+            for sink_node in self.sink_nodes[:i]:
+                if self.distance(current_sink_node.get_location(),sink_node.get_location()) < r:
+                    current_sink_node_nbrhood.add(sink_node)
+            
+            sink_node_neighborhood.append((current_sink_node,current_sink_node_nbrhood))
+            
+        # delete those sink node that are already part of other. 
+        # Add nbrhood with more then 2 neighbors to high and other to low
+        high_density_neighborhood: List[Set[SinkNode]] = []
+        low_density_neighborhood: List[Set[SinkNode]] = []
+        for curr_idx,curr_sink_node,current_sink_node_nbrhood in enumerate(sink_node_neighborhood):
+            include = True
+            for idx,_,sink_node_nbrhood in enumerate(sink_node_neighborhood):
+                if idx != curr_idx and len(current_sink_node_nbrhood) == len(current_sink_node_nbrhood.intersection(sink_node_nbrhood)):
+                    include = False
+        if include:
+            if len(current_sink_node_nbrhood) > 2:
+                high_density_neighborhood.append((curr_sink_node,current_sink_node_nbrhood))
+            else:
+                low_density_neighborhood.append((curr_sink_node,current_sink_node_nbrhood))
+        
+        return high_density_neighborhood,low_density_neighborhood
 
     def generate_random_point(self) -> Tuple[float, float]:
         """generate a random point for misn
