e81c553f5a3878b9254bc9352249834fae95c527
psn0w <08yaksh08@gmail.com>
2023-11-26 03:00:49 +0530

    Started with gui

---
+++ gui.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+
+from src.closed_loop.misn import MISN
+from src.constants import CONFIG
+
+
+def draw_triangle(canvas, x, y, size=20):
+    x1, y1 = x - size / 2, y + size / 2
+    x2, y2 = x + size / 2, y + size / 2
+    x3, y3 = x, y - size / 2
+
+    canvas.create_polygon(x1, y1, x2, y2, x3, y3, fill="blue")
+
+
+if __name__ == "__main__":
+    edge_length = CONFIG["max_dist_btw_sink_node"] * CONFIG['num_sink_node'] * 10
+    misn = MISN()
+
+    root = tk.Tk()
+    root.title("Moveable Square and Circle on Triangles")
+
+    canvas = tk.Canvas(
+        root,
+        width=edge_length,
+        height=edge_length,
+        bg="white",
+    )
+    canvas.pack()
+
+    # Draw triangles at specified coordinates
+    for node in misn.sink_nodes:
+        draw_triangle(canvas, *node.get_cords())
+
+    root.mainloop()
